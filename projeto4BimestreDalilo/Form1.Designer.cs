da55b1006555bd122b2c8b46ee85ac4640cdf9f5
Vinicius Cazano <vinicius.cazano@gmail.com>
2019-11-01 07:41:53 -0300

    Segundo commit teste de grid

--- projeto4BimestreDalilo/Form1.Designer.cs
+++ projeto4BimestreDalilo/Form1.Designer.cs
@@ -29,32 +29,75 @@
         private void InitializeComponent()
         {
             this.btnJogar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblBandeirasText = new System.Windows.Forms.Label();
+            this.lblBandeiras = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnJogar
             // 
-            this.btnJogar.Location = new System.Drawing.Point(316, 293);
+            this.btnJogar.Location = new System.Drawing.Point(445, 365);
             this.btnJogar.Name = "btnJogar";
             this.btnJogar.Size = new System.Drawing.Size(75, 23);
             this.btnJogar.TabIndex = 0;
             this.btnJogar.Text = "Jogar";
             this.btnJogar.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(619, 336);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            // 
+            // lblBandeirasText
+            // 
+            this.lblBandeirasText.AutoSize = true;
+            this.lblBandeirasText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBandeirasText.Location = new System.Drawing.Point(560, 406);
+            this.lblBandeirasText.Name = "lblBandeirasText";
+            this.lblBandeirasText.Size = new System.Drawing.Size(109, 24);
+            this.lblBandeirasText.TabIndex = 2;
+            this.lblBandeirasText.Text = "Bandeiras:";
+            // 
+            // lblBandeiras
+            // 
+            this.lblBandeiras.AutoSize = true;
+            this.lblBandeiras.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBandeiras.Location = new System.Drawing.Point(675, 406);
+            this.lblBandeiras.Name = "lblBandeiras";
+            this.lblBandeiras.Size = new System.Drawing.Size(51, 24);
+            this.lblBandeiras.TabIndex = 3;
+            this.lblBandeiras.Text = "num";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBandeiras);
+            this.Controls.Add(this.lblBandeirasText);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnJogar);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblBandeirasText;
+        private System.Windows.Forms.Label lblBandeiras;
     }
 }
 
--- projeto4BimestreDalilo/Form1.cs
+++ projeto4BimestreDalilo/Form1.cs
@@ -12,9 +12,101 @@ namespace projeto4BimestreDalilo
 {
     public partial class Form1 : Form
     {
+        int linha = 5;
+        int coluna = 5;
+        int bandeiras = 0;
+        int[,] matriz = new int[2, 2];
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.Columns.Add("columUm", "");
+            dataGridView1.Columns.Add("columDois", "");
+            dataGridView1.Columns.Add("columTres", "");
+            dataGridView1.Columns.Add("columQuatro", "");
+            dataGridView1.Columns.Add("columCinco", "");
+
+            dataGridView1.Rows.Add("            ", "        ", "       ", "3", "          ");
+            dataGridView1.Rows.Add("5", "6", "7", "8", "9");
+            dataGridView1.Rows.Add("10", "11", "12", "13", "14");
+            dataGridView1.Rows.Add("15", "16", "17", "18", "19");
+            dataGridView1.Rows.Add("20", "21", "22", "23", " ");
+        }
+        
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (! dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.Equals("              S"))
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "         Bandeira";
+                bandeiras++;
+                lblBandeiras.Text = bandeiras.ToString();
+            }
+            
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //int contador = 0;
+            //for (int a = 0; a < linha; a++)
+            //{
+            //    for (int b = 0; b < coluna; b++)
+            //    {
+            //        Console.Write(contador+" ");
+            //        contador++;
+            //    }
+            //    Console.WriteLine("");
+            //}
+            List<int> bomba = new List<int>();
+            bomba.Add(24);
+            bomba.Add(17);
+            bomba.Add(3);
+            int contador = 0;
+            if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.Equals("         Bandeira"))
+            {
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "              S";
+                bandeiras-=2;
+                lblBandeiras.Text = bandeiras.ToString();
+            }
+                
+
+            for (int a = 0; a < linha; a++)
+            {
+                for (int b = 0; b < coluna; b++)
+                {
+                    //verifica se a bomba esta na celula clicada
+                    foreach (int bomb in bomba)
+                    {
+                        if (contador == bomb)
+                        {
+                            if (e.RowIndex == a && e.ColumnIndex == b)
+                            {//Se estiver o mesmo mostra onde esta todas as bombas e acaba o jogo
+                                foreach (int bombEstorado in bomba)
+                                {
+                                    int contEstorado = 0;
+                                    for (int aEstorado = 0; aEstorado < linha; aEstorado++)
+                                    {
+                                        for (int bEstorado = 0; bEstorado < coluna; bEstorado++)
+                                        {
+                                            if (contEstorado == bombEstorado)
+                                            {
+                                                dataGridView1.Rows[aEstorado].Cells[bEstorado].Value = "              B";
+                                            }
+                                            contEstorado++;
+                                        }
+                                    }
+                                }
+                                dataGridView1.Enabled = false;
+                            }
+
+                        }
+
+                    }
+                    contador++;
+                }
+            }//fim dos for 
         }
     }
 }
